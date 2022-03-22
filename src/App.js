cdc09908f32ace02c251c3f55b5b317ccdd7b1e1
Raymond Cuya <raymondcuya@gmail.com>
2022-03-22 20:53:09 +1100

    added sign in route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.components';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
