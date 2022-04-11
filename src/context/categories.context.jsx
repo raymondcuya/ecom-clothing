ef1c062c4d58e64099b4f15cbb9677b2f22f8e06
Raymond Cuya <raymondcuya@gmail.com>
2022-04-11 20:40:42 +1000

    renamed products to categories

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useState } from 'react';
+
+import SHOP_DATA from '../shop-data.js';
+import {
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
+} from '../utils/firebase/firebase.utils.js';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  // create categories document
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
