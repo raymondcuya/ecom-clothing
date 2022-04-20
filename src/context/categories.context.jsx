258989c2b280a23297c814bee0fe9c6990c1e4a6
Raymond Cuya <raymondcuya@gmail.com>
2022-04-20 20:09:26 +1000

    refactored and removed unncessary files

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,10 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 
-import SHOP_DATA from '../shop-data.js';
-import {
-  addCollectionAndDocuments,
-  getCategoriesAndDocuments,
-} from '../utils/firebase/firebase.utils.js';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
@@ -12,15 +8,10 @@ export const CategoriesContext = createContext({
 
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
-  // create categories document
-  // useEffect(() => {
-  //   addCollectionAndDocuments('categories', SHOP_DATA);
-  // }, []);
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
       setCategoriesMap(categoryMap);
     };
 
