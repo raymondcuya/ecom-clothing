d250080b0d8bbb7e8d244fcc349945486705f9d9
Raymond Cuya <raymondcuya@gmail.com>
2022-04-22 22:40:08 +1000

    product card scss to jsx

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,14 @@
 import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
+
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-import './product-card.styles.scss';
+
+import {
+  Footer,
+  Name,
+  Price,
+  ProductCartContainer,
+} from './product-card.styles';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -10,19 +17,19 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className="product-card-container">
+    <ProductCartContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button
         buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to cart
       </Button>
-    </div>
+    </ProductCartContainer>
   );
 };
 
