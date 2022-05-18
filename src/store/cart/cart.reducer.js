c0c12cbc1cd6bcdbe6768cdc301fd4a000da4272
Raymond Cuya <raymondcuya@gmail.com>
2022-05-18 19:17:25 +1000

    setting initial values for count and total

--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -3,8 +3,6 @@ import { CART_ACTION_TYPES } from './cart.types';
 const CART_INITIAL_STATE = {
   isCartOpen: false,
   cartItems: [],
-  cartCount: 0,
-  cartTotal: 0,
 };
 
 export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
