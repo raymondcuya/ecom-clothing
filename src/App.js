aeef473bea1e2826b0839c1389486749e80a84e6
Raymond Cuya <raymondcuya@gmail.com>
2022-05-19 20:45:18 +1000

    updated for saga

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import {
-  createUserDocumentFromAuth,
-  getCurrentUser,
-  onAuthStateChangedListener,
-} from './utils/firebase/firebase.utils';
-
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
-import { setCurrentUser } from './store/user/user.action';
+import { checkUserSession } from './store/user/user.action';
 import { useDispatch } from 'react-redux';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user));
+    dispatch(checkUserSession());
   }, []);
 
   return (
